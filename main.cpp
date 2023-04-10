85a1793f7e91a742cc3e84e878191b79768d2e6b
i-am-tost <sidorchuk.me@phystech.edu>
2023-04-10 23:40:40 +0300

    lodsb/stosb to be added

--- main.cpp
+++ main.cpp
@@ -4,5 +4,8 @@ extern "C" void _printf (const char* fmt, ...);
 int main () {
 
     _printf ("%b\nit work\n%o%%\n\n", 0x88, 01234);
+    int a = 0;
+    a += 14;
+    printf ("\n\n\n%d\n\nlol", a);
     return 0;
 }
